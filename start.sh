8017ed49ba257d792e125c81a7c1f24991873800
Marc Wäckerlin <marc@tech.swisssign.net>
2016-04-04 16:46:10 +0200

    seems to work this way

--- start.sh
+++ start.sh
@@ -167,15 +167,12 @@ for name in $(env | sed -n 's/_PORT_.*_TCP_ADDR=.*//p' | sort | uniq); do
     cmd="location ${fromlocation}/ {
     include proxy.conf;
     set \$fixed_destination \$http_destination;
-    if ( \$http_destination ~* ^http://([^/]*)$frombase(.*)\$ ) {
-      set \$fixed_destination http://\$1\$2;
-    }
-    if ( \$http_destination ~* ^https://([^/]*)$frombase(.*)\$ ) {
-      set \$fixed_destination http://\$1\$2;
+    if ( \$http_destination ~* ^https(.*)\$ ) {
+      set \$fixed_destination http\$1;
     }
     proxy_set_header Destination \$fixed_destination;
     if (\$request_method ~ ^COPY\$) {
-      rewrite $frombase/(.*) /\$1 break;
+      rewrite $frombase/(.*) $frombase/\$1 break;
     }
     #if ( \$host != '${server}' ) {
     #  rewrite ^/(.*)$ \$scheme://${server}${fromlocation}/\$1 permanent;
