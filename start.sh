4d7b00405ce908a611b55a711a2da713e5f070c3
Marc Wäckerlin <marc@waeckerlin.org>
2017-02-11 13:45:17 +0100

    bugfixes - fix logging

--- start.sh
+++ start.sh
@@ -223,8 +223,8 @@ fi
 # fix logging
 ! test -e /var/log/nginx/access.log || rm /var/log/nginx/access.log
 ! test -e /var/log/nginx/error.log || rm /var/log/nginx/error.log
-ln -sf /proc/1/fd/1 /var/log/nginx/access.log
-ln -sf /proc/1/fd/2 /var/log/nginx/error.log
+ln -sf /proc/self/fd/1 /var/log/nginx/access.log
+ln -sf /proc/self/fd/2 /var/log/nginx/error.log
 
 # run webserver
 eval $proxycmd
